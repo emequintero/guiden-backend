e043253ed21b6ac26e0f6202e62baddd240a6ad0
Matthias Quintero <matthiasquintero@gmail.com>
2020-04-29 15:46:43 -0600

    successfully execute djikstra on a 2d matrix and return visitedNodesInOrder as well as shortestPath for animations

--- src/main/java/com/hylicmerit/controllers/AlgorithmController.java
+++ src/main/java/com/hylicmerit/controllers/AlgorithmController.java
@@ -4,33 +4,35 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hylicmerit.algorithms.DjikstraHelper;
-import com.hylicmerit.models.Animation;
 import com.hylicmerit.models.Node;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/algorithm")
+@RequestMapping(value = "/algorithm", produces = "application/json")
 public class AlgorithmController {
 	ObjectMapper mapper = new ObjectMapper();
 
-	@PostMapping("/dijkstra")
-	public ResponseEntity<String> dijkstra(@PathVariable Node start, 
-			@RequestBody int[][] graph)
+	@GetMapping("/dijkstra")
+	public ResponseEntity<String> dijkstra(
+			@RequestParam("start") int[] start,
+			@RequestParam("end") int[] end,
+			@RequestParam("rows") int rows,
+			@RequestParam("columns") int columns)
 			throws JsonProcessingException {
-		DjikstraHelper.resetAnimations();
-		int distance = DjikstraHelper.djikstra(graph, start);
-		List<Animation> animations = DjikstraHelper.getAnimations();
+		List<Node> visitedNodesInOrder = DjikstraHelper.djikstra(rows, columns, start, end);
+		List<Node> shortestPath = DjikstraHelper.getShortestPathAnimation();
 		return ResponseEntity.ok().body(
-				"{\"shortestDistance\" : " + mapper.writeValueAsString(distance) + "," + 
-				"\"animations\" : " + mapper.writeValueAsString(animations) + "}");
+				"{\"shortestDistance\" : " 
+						+ mapper.writeValueAsString(visitedNodesInOrder) + "," +
+				"\"shortestPath\" : " 
+						+ mapper.writeValueAsString(shortestPath) + "}");
 	}
 }
--- src/main/java/com/hylicmerit/models/Node.java
+++ src/main/java/com/hylicmerit/models/Node.java
@@ -1,36 +1,93 @@
 package com.hylicmerit.models;
 
-public class Node {
-	private int data;
-	private int id;
+public class Node implements Comparable<Node>{
+	private Integer column;
+	private Integer row;
+	private Integer distance = Integer.MAX_VALUE;
+	private Boolean isFinish = false;
+	private Boolean isStart = false;
+	private Boolean isWall = false;
+	private Boolean isVisited = false;
+	private Node previousNode = null;
 
-	public Node(int id, int data) {
-		super();
-		this.data = data;
-		this.id = id;
+	public Node(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
 	}
 
-	public int getData() {
-		return data;
+	public Integer getColumn() {
+		return column;
 	}
 
-	public void setData(int data) {
-		this.data = data;
+	public void setColumn(Integer column) {
+		this.column = column;
 	}
 
-	public int getId() {
-		return id;
+	public Integer getRow() {
+		return row;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public Integer getDistance() {
+		return distance;
+	}
+
+	public void setDistance(Integer distance) {
+		this.distance = distance;
+	}
+
+	public Node getPreviousNode() {
+		return previousNode;
+	}
+
+	public void setPreviousNode(Node previousNode) {
+		this.previousNode = previousNode;
+	}
+
+	public Boolean isFinish() {
+		return isFinish;
+	}
+
+	public void setFinish(Boolean isFinish) {
+		this.isFinish = isFinish;
+	}
+
+	public Boolean isStart() {
+		return isStart;
+	}
+
+	public void setStart(Boolean isStart) {
+		this.isStart = isStart;
+	}
+
+	public Boolean isWall() {
+		return isWall;
+	}
+
+	public void setWall(Boolean isWall) {
+		this.isWall = isWall;
+	}
+
+	public Boolean isVisited() {
+		return isVisited;
+	}
+
+	public void setVisited(Boolean isVisited) {
+		this.isVisited = isVisited;
 	}
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + data;
+		final Integer prime = 31;
+		Integer result = 1;
+		result = prime * result + column;
+		result = prime * result + (isFinish ? 1231 : 1237);
+		result = prime * result + (isStart ? 1231 : 1237);
+		result = prime * result + (isWall ? 1231 : 1237);
+		result = prime * result + row;
 		return result;
 	}
 
@@ -43,14 +100,29 @@ public class Node {
 		if (getClass() != obj.getClass())
 			return false;
 		Node other = (Node) obj;
-		if (data != other.data)
+		if (column != other.column)
+			return false;
+		if (isFinish != other.isFinish)
+			return false;
+		if (isStart != other.isStart)
+			return false;
+		if (isWall != other.isWall)
+			return false;
+		if (row != other.row)
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Node [data=" + data + ", id=" + id + "]";
+		return "Node [column=" + column + ", row=" + row + ", distance=" + distance + ", isFinish=" + isFinish
+				+ ", isStart=" + isStart + ", isWall=" + isWall + ", isVisited=" + isVisited + ", previousNode="
+				+ previousNode + "]";
 	}
-	
+
+	@Override
+	public int compareTo(Node next) {
+		return this.getDistance().compareTo(next.getDistance());
+	}
+
 }
